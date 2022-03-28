e4c563ab054b1ad4482fbad209c7267203fe29e4
seungjoonoh <cyanosj128@gmail.com>
2022-03-29 00:02:26 +0900

    1829 코드 정리

--- algorithm/1829.js
+++ algorithm/1829.js
@@ -8,12 +8,16 @@ function solution(m, n, picture) {
 	let area = 0;
 	let largest = 0;
 	
-	for (let i = 0; i < picture.length; i++) {
-		for (let j = 0; j < picture[i].length; j++) {
-			const color = picture[i][j];
-			if (color === 0 || visitedPicture[i][j]) continue;
-			const count = recur(i, j, picture, visitedPicture);
-			console.log(count);
+	for (let y = 0; y < picture.length; y++) {
+		for (let x = 0; x < picture[y].length; x++) {
+			if (picture[y][x] === 0) continue;
+			
+			// visited의 순서가 중요!
+			// 방문 이전에 마크
+			if (visitedPicture[y][x]) continue;
+			visitedPicture[y][x] = true;
+			
+			const count = recur(y, x, picture, visitedPicture);
 			if (count > largest) largest = count;
 			area++
 		}
@@ -21,20 +25,24 @@ function solution(m, n, picture) {
 	return [area, largest];
 }
 
-function recur(m, n, picture, visitedPicture, count = 0) {
-	visitedPicture[m][n] = true;
-	const color = picture[m][n];
-	let c = 0;
+function recur(y, x, picture, visitedPicture) {
+	let c = 1;
 	for (let i = 0; i < 4; i++) {
-		const nm = m + dx[i];
-		const nn = n + dy[i];
-		if (nm < 0 || nn < 0 || nm >= picture.length || nn >= picture[0].length || visitedPicture[nm][nn] || picture[nm][nn] !== color) continue;
-		c += recur(nm, nn, picture, visitedPicture, count + 1);
+		const ny = y + dx[i];
+		const nx = x + dy[i];
+		
+		// Range check
+		if (!visitedPicture[ny] || visitedPicture[ny][nx] == null) continue;
+		// Data check
+		if (visitedPicture[ny][nx] || picture[ny][nx] !== picture[y][x]) continue;
+		
+		// visited의 순서가 중요!
+		// 방문 이전에 마크
+		visitedPicture[ny][nx] = true;
+		c += recur(ny, nx, picture, visitedPicture);
 	}
-	// console.log(c);
-	return count + c;
+	return c;
 }
 
 console.log(solution(6, 4, [[1, 1, 1, 0], [1, 2, 2, 0], [1, 0, 0, 1], [0, 0, 0, 1], [0, 0, 0, 3], [0, 0, 0, 3]]));
-// console.log(solution(3,3, [[1,1,2,2],[1,2,2,3],[2,1,3,2],[2,2,3,3]]));
-// console.log(solution(100,97, [[1,1,100,97]]));
+console.log(solution(3, 3, [[0, 1, 0], [1, 1, 1], [0, 1, 0]]));
