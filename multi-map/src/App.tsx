76f2245f13af11d440da3b0de523686e823c92dd
seungjoonoh <cyanosj128@gmail.com>
2022-03-29 23:38:57 +0900

    test maps removed

--- multi-map/src/App.tsx
+++ multi-map/src/App.tsx
@@ -3,16 +3,10 @@ import './App.css'
 import MapControlPanel from "./components/MapControlPanel";
 import MapSwitcher from './components/MapSwitcher'
 import {Coordinate, MapType} from "./types";
-import TestGoogleMap from "./components/TestGoogleMap";
-import TestMapControlPanel from "./components/TestMapControlPanel";
-
-export type TestMode = 'EDIT' | 'FINISHED';
 
 function App() {
   const [mapType, setMapType] = useState<MapType>('NAVER');
   const [center, setCenter] = useState<Coordinate | null>(null);
-  
-  const [mode, setMode] = useState<TestMode>('EDIT');
   
   return (
     <div className="App">
